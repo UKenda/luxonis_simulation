6fbf9dd7ed2b8e61cb46064078603164353c619e
urban <urban.kenda@gmail.com>
2022-10-14 19:00:20 +0200

    Created a dummy_robot simulations in gazebo works

--- launch/robot_publisher.launch.py
+++ launch/robot_publisher.launch.py
@@ -29,7 +29,8 @@ def generate_launch_description():
         parameters=[params]
     )
 
-
+    joint_state_publisher = Node(package='joint_state_publisher', executable='joint_state_publisher',
+                    output='screen')
 
     # Launch!
     return LaunchDescription([
@@ -38,5 +39,6 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        joint_state_publisher
     ])
